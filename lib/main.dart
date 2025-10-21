8d13b121ba407a4707aa1bfa6eb0f0849e6060ee
sseid4 <sseid4@student.gsu.edu>
2025-10-21 13:00:22 -0400

    Design two screens folder screen and card screens

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cards App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const FoldersScreen(),
+    );
+  }
+}
+
+
+class Folder {
+  final String id;
+  final String name;
+
+  const Folder({required this.id, required this.name});
+}
+
+// Screen 1: Folders Screen
+class FoldersScreen extends StatefulWidget {
+  const FoldersScreen({super.key});
+
+  @override
+  State<FoldersScreen> createState() => _FoldersScreenState();
+}
+
+class _FoldersScreenState extends State<FoldersScreen> {
+
+  final List<Folder> folders = const [
+    Folder(id: '1', name: 'Folder 1'),
+    Folder(id: '2', name: 'Folder 2'),
+    Folder(id: '3', name: 'Folder 3'),
+    Folder(id: '4', name: 'Folder 4'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Folders')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+            childAspectRatio: 1.2,
+          ),
+          itemCount: folders.length,
+          itemBuilder: (context, index) {
+            final folder = folders[index];
+            return _buildFolderCard(context, folder);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFolderCard(BuildContext context, Folder folder) {
+    return Card(
+      elevation: 2,
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CardsScreen(folder: folder),
+            ),
+          );
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.folder, size: 48, color: Colors.grey),
+              const SizedBox(height: 12),
+              Text(
+                folder.name,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Screen 2: Cards Screen
+class CardsScreen extends StatelessWidget {
+  final Folder folder;
+
+  const CardsScreen({super.key, required this.folder});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(folder.name)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.inbox, size: 80, color: Colors.grey[400]),
+            const SizedBox(height: 16),
+            Text(
+              'No cards yet',
+              style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
